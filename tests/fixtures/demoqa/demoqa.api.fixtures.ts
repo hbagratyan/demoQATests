ed44dab94818b3313bb6580bda1ed4307597e5cb
g.bagratian <g.bagratian@sbereducation.ru>
2026-04-14 23:01:21 +0300

    API tests refactor: code dubs deleted, user creation fixture added

--- tests/fixtures/demoqa/demoqa.api.fixtures.ts
+++ tests/fixtures/demoqa/demoqa.api.fixtures.ts
@@ -7,6 +7,12 @@ export const test = base.extend<{
     userClient: UserClient;
     bookClient: BookClient;
     dataGenerator: DataGenerator;
+    authorizedUser: {
+        userId: string;
+        token: string;
+        username: string;
+        password: string;
+    };
 }>({
     userClient: async ({ request }, use) => {
         await use(new UserClient(request));
@@ -18,5 +24,29 @@ export const test = base.extend<{
 
     dataGenerator: async ({}, use) => {
         await use(new DataGenerator());
+    },
+
+    authorizedUser: async ({ userClient, dataGenerator }, use) => {
+        const username = dataGenerator.userName();
+        const password = dataGenerator.password();
+
+        const userResponse = await userClient.createUser(username, password);
+        if (userResponse.status !== 201) {
+            throw new Error('Failed to create user');
+        }
+
+        const tokenResponse = await userClient.generateToken(username, password);
+        if (tokenResponse.status !== 200) {
+            throw new Error('Failed to generate token');
+        }
+
+        const user = {
+            userId: userResponse.body.userID,
+            token: tokenResponse.body.token,
+            username,
+            password
+        };
+
+        await use(user);
     }
 });
